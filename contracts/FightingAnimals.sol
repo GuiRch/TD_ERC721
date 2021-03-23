68ed8c03887c321f388be82953aa533d9bf182d0
GuiRch <guillaumericher@outlook.fr>
2021-03-23 20:35:12 +0100

    ajout de la fonction create animal

--- contracts/FightingAnimals.sol
+++ contracts/FightingAnimals.sol
@@ -3,13 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/math/SafeMath.sol';
+
 //import "@openzeppelin/contracts/token/ERC721/IERC721Metadata.sol";
 
 contract FightingAnimals is ERC721, Ownable {
+
+    using SafeMath for uint;
+    using Math for uint;
+
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("CryptoPoulpe", "PLP") {}
+    constructor() public ERC721("CryptoPoulpe", "PLP") {
+        //_numToken = 0;
+    }
 
     mapping( address => bool) registedBreeder;
     event registerNewBreeder( address indexed account);
@@ -34,29 +43,35 @@ contract FightingAnimals is ERC721, Ownable {
         return registedBreeder[_address];
     }
 
+    mapping(uint256 => CryptoPoulpe) public metadata;
 
-    function awardItem(address player, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
-    }
+    enum Color{green, red, blue, yellow, purple, pink}
+    enum Rarity{normal, rare, epic, legendary, godlike}
 
     struct CryptoPoulpe{
-        string name; // Name of the Item
-        string color;
-        uint tentacule; // Item Level
-        uint rarityLevel;  // 1 = normal, 2 = rare, 3 = epic, 4 = legendary
+        uint256 chance; // luckyness of our cryptopoulpe
+        uint256 smart; // intelligence of our cryptopoulpe
+        uint256 tentacule; // Item Level
+        Color color; // color of the crypto poulpe
+        Rarity rarity;  // 1 = normal, 2 = rare, 3 = epic, 4 = legendary
+    }
+
+    
+    function createAnimal() private returns(bool){
+        // this function will choose random characteritics for our new animal
+        Color color = Color(random(uint(Color.GROUND)));
+        Rarity rarity = Rarity(random(uint(Rarity.GROUND)));
+        uint256 chance = random(100);
+        uint256 smart = random(20);
+        uint256 tentacule = random(10);
+        CryptoPoulpe memory randomCryptoPoulpe = CryptoPoulpe(chance, smart, tentacule, color, rarity);
+        //metadata[_numToken] = randomCryptoPoulpe;
+
     }
 
     // here the function to create a new animal 
-    function declareAnimal(address receiver, string memory tokenURI) external onlyOwner returns (uint256){
+    function declareAnimal(address receiver, string calldata tokenURI) external onlyOwner returns (uint256){
+        createAnimal();
         _tokenIds.increment();
 
         uint256 newAnimal = _tokenIds.current();
@@ -70,7 +85,4 @@ contract FightingAnimals is ERC721, Ownable {
         _burn(tokenID);
     }
 
-    function breedAnimal(address breeder, uint256 tokenId) public {
-
-    }
 }
